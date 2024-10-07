1b7c8e55bcb77b1afa8f5f5b8788918e4b536828
Ahmet <Ahmet@DESKTOP-6Q0TJG5>
2024-10-07 19:14:42 +0300

    Add project files.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,11 @@
+﻿using DependencyInjection;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ITeacher teacher = new Teacher("Sertan", "Bozkuş");
+        ClassRoom classRoom = new ClassRoom(teacher);
+        classRoom.GetTeacherInfo();
+    }
+}
